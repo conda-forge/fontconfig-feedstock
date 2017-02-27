a32428b42eb2c25187b835deeeca8aea9f919b10
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-02-27 13:18:41 +0100

    Added call for scripts/activate.sh / scripts/deactivate.sh to activate/deactivate system environment variables that some software need.

--- recipe/build.sh
+++ recipe/build.sh
@@ -28,3 +28,12 @@ rm -Rf "${PREFIX}/var/cache/fontconfig"
 # Leave cache directory, in case it's needed
 mkdir -p "${PREFIX}/var/cache/fontconfig"
 touch "${PREFIX}/var/cache/fontconfig/.leave"
+
+#Add FONTCONFIG_PATH and FONTCONFIG_FILE to the enviroment variables
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/fontconfig-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/fontconfig-deactivate.sh

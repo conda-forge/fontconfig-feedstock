23d61475a195cfdf6f47b3108dabe9fd0c899367
Joachim Wolff <wolffj@informatik.uni-freiburg.de>
2017-02-27 13:16:44 +0100

    Added scripts to activate / deactivate FONTCONFIG_PATH and FONTCONFIG_FILE

---
+++ recipe/scripts/activate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [[ -z "$FONTCONFIG_FILE" ]]; then
+    export FONTCONFIG_FILE=\$CONDA_PREFIX/etc/fonts/fonts.conf
+    export FONTCONFIG_PATH=\$CONDA_PREFIX/etc/fonts/
+fi
---
+++ recipe/scripts/deactivate.sh
@@ -0,0 +1,4 @@
+if [[ -n "$FONTCONFIG_FILE" ]]; then
+	unset FONTCONFIG_FILE
+	unset FONTCONFIG_PATH
+fi
